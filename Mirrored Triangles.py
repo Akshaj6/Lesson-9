1733246b8a01a619646a1049de4899698e7c958f
Akshaj6 <akshaj.samaria@gmail.com>
2025-04-13 14:31:17 +0530

    28

--- Mirrored Triangles.py
+++ Mirrored Triangles.py
@@ -1,12 +1,13 @@
 height = int(input("Enter the height :"))
-row = 1
-while row <= height:
-    col = 1
-    while col <= height:
-        print(" ", end = "" )
+row = 0
+while row < height:
+    col = 0
+    while col < row:
+        print(" ", end= "" )
         col = col + 1
-    col = 1
-    while col <= row:
-        print("*", end = "")
+    col = 0
+    while col < (height - row):
+        print("*", end= "")
+        col = col + 1
     print()
     row = row + 1
